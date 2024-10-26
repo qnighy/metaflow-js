78604fa41cea5a4e36626eb954df3a9061373c7e
Luma <world@luma.email>
2024-10-27 03:23:31 +0900

    fix test title and class scopes

--- exception.test.ts
+++ exception.test.ts
@@ -1,12 +1,6 @@
 import { assertEquals } from "jsr:@std/assert";
 import { Try } from "./exception.ts";
 
-class DemoError extends Error {
-  constructor(demoRequiredArg: string) {
-    super(`DemoError: ${demoRequiredArg}`);
-  }
-}
-
 Deno.test("Try pick Error", () => {
   const result = Try(() => {
     throw new Error();
@@ -21,7 +15,12 @@ Deno.test("Try pick Error and catch subclass TypeError", () => {
   assertEquals(result, 42);
 });
 
-Deno.test("Try pick Error and catch subclass DemoError", () => {
+Deno.test("Try pick DemoError (user class)", () => {
+  class DemoError extends Error {
+    constructor(demoRequiredArg: string) {
+      super(`DemoError: ${demoRequiredArg}`);
+    }
+  }
   const result = Try(() => {
     throw new DemoError("demo");
   }).pick(DemoError).done(() => 42);
